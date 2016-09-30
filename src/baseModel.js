2a6d27cacaeaf6d4ab88c674414cc2c85ea16c7b
Francisco Diaz <jdiaz@estrasol.com.mx>
2016-09-30 13:37:58 -0500

    Se crea el modelo y una prueba de una facotory reutilizable

--- src/baseModel.js
+++ src/baseModel.js
@@ -1,1 +1,9 @@
-//Aquí ira basemodel
+!function(){
+    var devTics = angular.module('devtics-angular-model-base',[]);
+    devTics.factory('ModelBaseT1',function(){
+        var ModelBaseT1 = function () {
+            this.msj="Modelo Base";
+        };
+        return ModelBaseT1;
+    });
+}();
